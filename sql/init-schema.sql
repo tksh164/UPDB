ea8ae08810a9f52a9d7c4ca12a7c2a3de57ccc73
Takeshi Katano <takatano@outlook.com>
2017-12-31 03:07:14 +0900

    Revert "Change schema of Logs table"
    This reverts commit cf04436d4336addbd866ba8e5b513d5a4e47ebb8.

--- sql/init-schema.sql
+++ sql/init-schema.sql
@@ -705,8 +705,8 @@ CREATE TABLE Logs
     -- The timestamp of the log record as UTC.
     TimestampUtc DATETIME2(0) NOT NULL,
 
-    -- The source of log.
-    Message NVARCHAR(20) NOT NULL,
+    -- The process target ID.
+    ProcessTargetId INT FOREIGN KEY REFERENCES ProcessTargets (Id),
 
     -- The classification of the log record.
     ClassificationId SMALLINT FOREIGN KEY REFERENCES LogClassifications (Id),
@@ -715,10 +715,7 @@ CREATE TABLE Logs
     Message NVARCHAR(400) NOT NULL,
 
     -- The log data.
-    Data NVARCHAR(4000) NOT NULL DEFAULT N'',
-
-    -- The process target ID. This may NULL, for example, the log is generated before the process target insertion.
-    ProcessTargetId INT
+    Data NVARCHAR(4000) NOT NULL DEFAULT N''
 
     PRIMARY KEY CLUSTERED (
         TimestampUtc ASC
