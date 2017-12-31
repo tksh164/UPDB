4845d2ea0c3c3d892535b0a576ac72453a9a3757
Takeshi Katano <takatano@outlook.com>
2018-01-01 01:54:32 +0900

    Add logging module project

---
+++ UpdateProgramDB/Logging/Logger.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Reflection;
+using UpdateProgramDB.Models;
+
+namespace UpdateProgramDB.Logging
+{
+    public enum LogClassification : short
+    {
+        Information = 0,
+        Warning = 1,
+        Error = 2,
+        Debug = 3,
+    }
+
+    public static class Logger
+    {
+        public static void WriteLog(string message)
+        {
+            WriteLog(LogClassification.Information, message, null);
+        }
+
+        public static void WriteLog(LogClassification logClassification, string message)
+        {
+            WriteLog(logClassification, message, null);
+        }
+
+        public static void WriteLog(LogClassification logClassification, string message, string data)
+        {
+            using (var db = new UpdateProgramDbContext())
+            {
+                db.Logs.Add(new Log()
+                {
+                    TimestampUtc = DateTime.UtcNow,
+                    Source = Assembly.GetEntryAssembly().GetName().Name,
+                    ClassificationId = (short)logClassification,
+                    Message = message,
+                    Data = data,
+                });
+
+                db.SaveChanges();
+            }
+        }
+    }
+}
